9e28f827451b83a2ca84b88fbd3ef2580d54ac86
Benacsek Dorián <benacsekd07@gmail.com>
2024-02-13 14:59:20 +0100

    Slider

--- src/main/java/com/example/keringo/HelloApplication.java
+++ src/main/java/com/example/keringo/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.keringo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Keringő");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("ball1.png")));
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/keringo/HelloController.java
+++ src/main/java/com/example/keringo/HelloController.java
@@ -1,14 +1,33 @@
 package com.example.keringo;
 
+import javafx.animation.AnimationTimer;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    @FXML private Slider sSpeed;
+    @FXML private Slider sRadius;
+    @FXML private Label lbSpeed;
+    @FXML private Label lbRadius;
+    @FXML private Pane pn;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+   /*@FXML private void onSlider(){
+        lbSpeed.setText(String.format("Speed: %.0f",sSpeed.getValue()));
+
+    }*/
+
+    public void initialize(){
+        lbSpeed.textProperty().bind(Bindings.format("Speed: %.0f", sSpeed.valueProperty()));
+        lbRadius.textProperty().bind(Bindings.format("Radius: %.0f", sRadius.valueProperty()));
+        AnimationTimer at = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+
+            }
+        };
     }
 }
